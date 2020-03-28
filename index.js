15470359372f4ee7f56a4a40f382c1a172a00036
Sir Eric <echeungsk@gmail.com>
2020-03-28 20:07:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+let var1 = 2;
+let var2 = 31;
+let eight = 8;
+let twelve = 12
+
+let multiply = var1 * var2;
+let random = Math.random();
+let mod = eight * var1 % twelve;
+let max = Math.max(1, 5, 10, 20)
